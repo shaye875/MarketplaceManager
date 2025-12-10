3de5e3c0065486361753e30c2225dea72c2ab567
shaye <sm0548540902@gmail.com>
2025-12-10 12:33:25 +0200

    add functions: edit and show

--- utils/arreyUtils.js
+++ utils/arreyUtils.js
@@ -1,5 +1,6 @@
 import { products } from "../data/product.js";
 import readLineSync from "readline-sync"
+import { sortList } from "./sortArrey.js";
 
 export function addProduct(product) {
     try {
@@ -20,3 +21,29 @@ export function removeProduct() {
     }
     return false
 }
+
+export function editPrice(){
+    const id = readLineSync.question("whice id you want edit: ")
+    const price = readLineSync.question("which price: ")
+    const found = products.find((p)=>Number(p.id) === Number(id))
+    found.price = price
+}
+
+export function showByPriceOrder(){
+    let productSort = []
+    let pricres = products.map((p)=>{
+        return p.price
+    })    
+    sortList(pricres)
+ for (let i = 0;i<pricres.length;i++){
+    for (let j = 0;j<pricres.length;j++){     
+        if (Number(pricres[i]) === Number(products[j].price)){
+            productSort.push(products[j])
+            j = pricres.length
+        }
+    }
+}
+    console.table(productSort)
+}
+showByPriceOrder()
+
