c6c0626f35532b25b58c2f1e0f535d30153d5aea
shaye <sm0548540902@gmail.com>
2025-12-10 12:40:55 +0200

    add function:show by id

--- utils/arreyUtils.js
+++ utils/arreyUtils.js
@@ -16,34 +16,45 @@ export function removeProduct() {
     const id = readLineSync.question("which id you wont del: ")
     for (let p of products) {
         if (Number(p.id) === Number(id))
-        products.splice(products.indexOf(p),1)
+            products.splice(products.indexOf(p), 1)
         return true
     }
     return false
 }
 
-export function editPrice(){
+export function editPrice() {
     const id = readLineSync.question("whice id you want edit: ")
     const price = readLineSync.question("which price: ")
-    const found = products.find((p)=>Number(p.id) === Number(id))
+    const found = products.find((p) => Number(p.id) === Number(id))
     found.price = price
 }
 
-export function showByPriceOrder(){
+export function showByPriceOrder() {
     let productSort = []
-    let pricres = products.map((p)=>{
+    let pricres = products.map((p) => {
         return p.price
-    })    
+    })
     sortList(pricres)
- for (let i = 0;i<pricres.length;i++){
-    for (let j = 0;j<pricres.length;j++){     
-        if (Number(pricres[i]) === Number(products[j].price)){
-            productSort.push(products[j])
-            j = pricres.length
+    for (let i = 0; i < pricres.length; i++) {
+        for (let j = 0; j < pricres.length; j++) {
+            if (Number(pricres[i]) === Number(products[j].price)) {
+                productSort.push(products[j])
+                j = pricres.length
+            }
         }
     }
-}
     console.table(productSort)
 }
 
 
+export function showProductDetails() {
+    const id = readLineSync.question("wich id you want show: ")
+    for (let p of products) {
+        if (Number(id) === Number(p.id)) {
+            for (let key in p) {
+                console.log(`${key}: ${p[key]}`);
+            }
+
+        }
+    }
+}
